49246d94754991a6db6f034b3a03016493d87f9d
BleuxGenius <nicoleblack360@gmail.com>
2019-09-10 14:09:57 -0500

    Add files via upload

---
+++ Hobby Tracker Completed/Grail/Grail/CluesTableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Grail
+//
+//  Created by Danielle Blackwell on 8/13/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class CluesTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
